5a96bce5e62f2e1e4bf5919fe73405fcedaa1d3f
SaheedGitIvyTech <143648082+SaheedGitIvyTech@users.noreply.github.com>
2023-11-28 18:08:36 -0500

    NewAddonsSahed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomeWebPage from './Pages/HomeWebPage';
+import Flooring from './LinksToAllJobs/Flooring';
+import HomeSceurity from './LinksToAllJobs/HomeSecurity';
+import HouseKeeping from './LinksToAllJobs/HouseKeeping';
+import InteriorDesign from './LinksToAllJobs/InteriorDesign';
+import LandScaping from './LinksToAllJobs/Landscaping';
+import Mounting from './LinksToAllJobs/Mounting';
+import Plumbering from './LinksToAllJobs/Plumbering';
+import PoolMaint from './LinksToAllJobs/PoolMaint';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+      <div className="App">
+         <h1>Awaits Completion</h1>
+         <div id="page-body">
+            <Routes>
+               <Route path="/" element={<HomeWebPage />}/>
+               <Route path="/floor" element={<Flooring />}/>
+               <Route path="/security" element={<HomeSceurity />}/>
+               <Route path="/house" element={<HouseKeeping />}/>
+               <Route path="/interior" element={<InteriorDesign />}/>
+               <Route path="/land" element={<LandScaping />}/>
+               <Route path="/Mounting" element={<Mounting />}/>
+               <Route path="/Plumbering" element={<Plumbering />}/>
+               <Route path="/Pool" element={<PoolMaint />}/>
+
+            </Routes>
+         </div>
+      </div>
+   </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/LinksToAllJobs/Plumbering.js
@@ -0,0 +1,7 @@
+const Plumbering  = () => {
+  return(
+      <h1>This is the Plumbering Page!</h1>
+  )
+}
+
+export default Plumbering
